92d951ab8639874743df5ffd2bd8457e3089883b
Steven <delchambre.steven@gmail.com>
2020-07-29 19:24:59 +0200

    Commit

--- computed/app.js
+++ computed/app.js
@@ -10,7 +10,10 @@ let vm = new Vue({
                     return this.nom + ' ' + this.prenom
                 },
                 set: function(value){
-                    console.log(value)
+                    let test = value.split(' ')
+                    console.log(test)
+                    this.nom = test[0]
+                    this.prenom = test[1]
                 }
         }
     }
